2967c5a0e1c55825d6181b6655fda8e4285d28f2
Dhavish080 <dhavishsegobin69@gmail.com>
2024-12-28 16:29:23 +0400

    yooo

---
+++ assets/images/yolo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class my extends StatefulWidget {
+  const my({super.key});
+
+  @override
+  State<my> createState() => _myState();
+}
+
+class _myState extends State<my> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
