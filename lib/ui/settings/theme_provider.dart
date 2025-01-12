b9dfb922c3009278d789a1eded63fb2e8a9a5bd8
Dhavish080 <dhavishsegobin69@gmail.com>
2025-01-12 07:19:30 +0400

    commit

---
+++ lib/ui/settings/theme_provider.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeMode _themeMode = ThemeMode.light;
+
+  ThemeMode get themeMode => _themeMode;
+
+  bool get isDarkMode => _themeMode == ThemeMode.dark;
+
+  void toggleTheme(bool isDarkMode) {
+    _themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+}
+
+class MyThemes {
+  static final darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: Colors.black,
+    scaffoldBackgroundColor: Colors.grey[900],
+    cardColor: Colors.grey[850],
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.grey[850],
+      elevation: 0,
+    ),
+    textTheme: TextTheme(
+      bodyLarge: TextStyle(color: Colors.white),
+      bodyMedium: TextStyle(color: Colors.white70),
+    ),
+  );
+
+  static final lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.grey[200],
+    cardColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+    ),
+    textTheme: const TextTheme(
+      bodyLarge: TextStyle(color: Colors.black),
+      bodyMedium: TextStyle(color: Colors.black54),
+    ),
+  );
+}
